db34c3ae4ca2b8034c800376262a7a19531535a5
CloverMidori <youhuid05@gmail.com>
2024-03-25 02:56:51 +0900

    Add VRMImporter

--- Editor/Scripts/CustomPrefabUtility/VRMImporter.cs
+++ Editor/Scripts/CustomPrefabUtility/VRMImporter.cs
@@ -6,10 +6,8 @@ using System.IO;
 using System.Linq;
 using UniGLTF;
 using UnityEditor;
-using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.U2D;
 using UnityEngine.UIElements;
 using VRC.Core;
 using VRM;
@@ -26,21 +24,27 @@ namespace EAUploader.CustomPrefabUtility
             {
                 return;
             }
-            if (path.StartsWithUnityAssetPath())
+            else
             {
-                Debug.LogWarningFormat("disallow import from folder under the Assets");
-                return;
-            }
-            string vrmFolder = "Assets/VRM";
-            if (!AssetDatabase.IsValidFolder(vrmFolder))
-            {
-                AssetDatabase.CreateFolder("Assets", "VRM");
-            }
+                if (path.StartsWithUnityAssetPath())
+                {
+                    Debug.LogWarningFormat("disallow import from folder under the Assets");
+                    return;
+                }
+                string vrmFolder = "Assets/VRM";
+                if (!AssetDatabase.IsValidFolder(vrmFolder))
+                {
+                    AssetDatabase.CreateFolder("Assets", "VRM");
+                }
 
-            var prefabPath = Path.Combine(vrmFolder, Path.GetFileNameWithoutExtension(path) + ".prefab");
-            vrmAssetPostprocessor.ImportVrmAndCreatePrefab(path, UnityPath.FromUnityPath(prefabPath));
+                var prefabPath = Path.Combine(vrmFolder, Path.GetFileNameWithoutExtension(path) + ".prefab");
+                vrmAssetPostprocessor.ImportVrmAndCreatePrefab(path, UnityPath.FromUnityPath(prefabPath));
 
-            WaitForPrefabGeneration(prefabPath, () => VRMImporterWindow.ShowWindow(prefabPath));
+                WaitForPrefabGeneration(prefabPath, () =>
+                {
+                    VRMImporterWindow.ShowWindow(prefabPath);
+                });
+            }
         }
 
         private static void WaitForPrefabGeneration(string prefabPath, Action callback)
@@ -58,7 +62,7 @@ namespace EAUploader.CustomPrefabUtility
         }
     }
 
-    internal class VRMImporterWindow : EditorWindow
+    public class VRMImporterWindow : EditorWindow
     {
         private string prefabPath = "";
         SwayingObjectsConverterSetting swayingObjectsConverterSetting;
@@ -80,7 +84,77 @@ namespace EAUploader.CustomPrefabUtility
 
         public void CreateGUI()
         {
-            rootVisualElement.schedule.Execute(() => LanguageUtility.Localization(rootVisualElement)).Every(100);
+            rootVisualElement.styleSheets.Add(UI.EAUploader.styles);
+            rootVisualElement.styleSheets.Add(UI.EAUploader.tailwind);
+
+            rootVisualElement.schedule.Execute(() =>
+            {
+                LanguageUtility.Localization(rootVisualElement);
+            }).Every(100);
+
+            var visualTree = Resources.Load<VisualTreeAsset>("UI/Windows/VRMImporter");
+            visualTree.CloneTree(rootVisualElement);
+
+            rootVisualElement.Q<EnumField>("springBone").Init(SwayingObjectsConverterSetting.ConvertVrmSpringBonesAndVrmSpringBoneColliderGroups);
+            rootVisualElement.Q<EnumField>("springBone").RegisterCallback<ChangeEvent<Enum>>((evt) =>
+            {
+                swayingObjectsConverterSetting = (SwayingObjectsConverterSetting)evt.newValue;
+            });
+
+            rootVisualElement.Q<ShadowButton>("importButton").clicked += ImportVRMButtonClicked;
+
+            addedShouldersPositionYSlider = rootVisualElement.Q<Slider>("addedShouldersPositionY");
+
+            addedShouldersPositionYFiller = new VisualElement() { name = "addedShouldersPositionYFiller" };
+            addedShouldersPositionYFiller.AddToClassList("unity-slider__filler");
+            addedShouldersPositionYSlider.Q("unity-drag-container").Add(addedShouldersPositionYFiller);
+
+            addedShouldersPositionYFiller.SendToBack();
+            addedShouldersPositionYSlider.Q("unity-tracker").SendToBack();
+
+            addedShouldersPositionYDragger = addedShouldersPositionYSlider.Q("unity-dragger");
+
+            addedShouldersPositionYSlider.RegisterValueChangedCallback((evt) =>
+            {
+                rootVisualElement.Q<Label>("addedShouldersPositionYLabel").text = evt.newValue.ToString("F4");
+            });
+
+            addedArmaturePositionYSlider = rootVisualElement.Q<Slider>("addedArmaturePositionY");
+
+            addedArmaturePositionYFiller = new VisualElement() { name = "addedArmaturePositionYFiller" };
+            addedArmaturePositionYFiller.AddToClassList("unity-slider__filler");
+            addedArmaturePositionYSlider.Q("unity-drag-container").Add(addedArmaturePositionYFiller);
+
+            addedArmaturePositionYFiller.SendToBack();
+            addedArmaturePositionYSlider.Q("unity-tracker").SendToBack();
+
+            addedArmaturePositionYDragger = addedArmaturePositionYSlider.Q("unity-dragger");
+
+            addedArmaturePositionYSlider.RegisterValueChangedCallback((evt) =>
+            {
+                rootVisualElement.Q<Label>("addedArmaturePositionYLabel").text = evt.newValue.ToString("F4");
+            });
+        }
+
+        Slider addedShouldersPositionYSlider;
+        VisualElement addedShouldersPositionYFiller;
+        VisualElement addedShouldersPositionYDragger;
+
+        Slider addedArmaturePositionYSlider;
+        VisualElement addedArmaturePositionYFiller;
+        VisualElement addedArmaturePositionYDragger;
+
+        public void Update()
+        {
+            if (addedShouldersPositionYSlider != null)
+            {
+                addedShouldersPositionYFiller.style.width = new StyleLength(new Length((addedShouldersPositionYDragger.transform.position.x + 10) / addedShouldersPositionYSlider.worldBound.width * 100, LengthUnit.Percent));
+            }
+
+            if (addedArmaturePositionYSlider != null)
+            {
+                addedArmaturePositionYFiller.style.width = new StyleLength(new Length((addedArmaturePositionYDragger.transform.position.x + 10) / addedArmaturePositionYSlider.worldBound.width * 100, LengthUnit.Percent));
+            }
         }
 
         private void ImportVRMButtonClicked()
@@ -113,11 +187,11 @@ namespace EAUploader.CustomPrefabUtility
             GameObject prefabInstance = Duplicator.Duplicate(previousPrefab, prefabPath, new List<string> { "" }, true);
 
             var clips = VRMUtility.GetAllVRMBlendShapeClips(prefabInstance);
-            bool forQuest = rootVisualElement.Q<Toggle>("forQuest").value;
-            bool takingOverSwayingParameters = rootVisualElement.Q<Toggle>("takingOverSwayingParameters").value;
+            bool forQuest = rootVisualElement.Q<SlideToggle>("forQuest").value;
+            bool takingOverSwayingParameters = rootVisualElement.Q<SlideToggle>("takingOverSwayingParameters").value;
             float addedShouldersPositionY = rootVisualElement.Q<Slider>("addedShouldersPositionY").value;
             float addedArmaturePositionY = rootVisualElement.Q<Slider>("addedArmaturePositionY").value;
-            bool useShapeKeyNormalsAndTangents = rootVisualElement.Q<Toggle>("useShapeKeyNormalsAndTangents").value;
+            bool useShapeKeyNormalsAndTangents = rootVisualElement.Q<SlideToggle>("useShapeKeyNormalsAndTangents").value;
             Convert(
                 prefabInstance: prefabInstance,
                 clips: clips,
