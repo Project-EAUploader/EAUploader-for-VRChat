79f6f69509dff27fbac584667a83f7493fd2fd26
さぶ <fu-suke.tm@outlook.jp>
2024-04-27 23:52:51 +0900

    fix

--- Editor/Scripts/EAUInitializeOnLoadAttribute.cs
+++ Editor/Scripts/EAUInitializeOnLoadAttribute.cs
@@ -3,35 +3,38 @@ using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 
-[AttributeUsage(AttributeTargets.Method)]
-public class EAUInitializeOnLoadAttribute : Attribute
+namespace EAUploader
 {
-    public EAUInitializeOnLoadAttribute()
+    [AttributeUsage(AttributeTargets.Method)]
+    public class EAUInitializeOnLoadAttribute : Attribute
     {
-        var stackTrace = new System.Diagnostics.StackTrace();
-        var methodInfo = (MethodInfo)stackTrace.GetFrame(1).GetMethod();
-        EAUInitializeOnLoadHelper.AddMethod(this, methodInfo);
-    }
-}
-
-public static class EAUInitializeOnLoadHelper
-{
-    private static List<MethodInfo> methods = new List<MethodInfo>();
-
-    public static void AddMethod(EAUInitializeOnLoadAttribute attribute, MethodInfo methodInfo)
-    {
-        if (methodInfo != null)
+        public EAUInitializeOnLoadAttribute()
         {
-            methods.Add(methodInfo);
+            var stackTrace = new System.Diagnostics.StackTrace();
+            var methodInfo = (MethodInfo)stackTrace.GetFrame(1).GetMethod();
+            EAUInitializeOnLoadHelper.AddMethod(this, methodInfo);
         }
     }
 
-    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-    private static void Execute()
+    public static class EAUInitializeOnLoadHelper
     {
-        foreach (var method in methods)
+        private static List<MethodInfo> methods = new List<MethodInfo>();
+
+        public static void AddMethod(EAUInitializeOnLoadAttribute attribute, MethodInfo methodInfo)
         {
-            method.Invoke(null, null);
+            if (methodInfo != null)
+            {
+                methods.Add(methodInfo);
+            }
+        }
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void Execute()
+        {
+            foreach (var method in methods)
+            {
+                method.Invoke(null, null);
+            }
         }
     }
 }
